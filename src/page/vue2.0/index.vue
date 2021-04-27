07e181bb6937d97db57988d240cd6ce9d176e096
QieJiZhuang <2533510514@qq.com>
2021-04-27 09:53:15 +0800

    Initial commit

---
+++ src/page/vue2.0/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div v-cloak >
+		{{number +1}}
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            number: 1
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
